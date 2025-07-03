3e4aad9e740ffe4ed48fbbebc3cae1e07efe4add
AnishMorde <anishmorde@gmail.com>
2025-07-03 14:55:50 +0530

    Restclient changes

--- src/test/java/com/example/anish/prodapp/ProdappApplicationTests.java
+++ src/test/java/com/example/anish/prodapp/ProdappApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.anish.prodapp;
 
+import com.example.anish.prodapp.client.EmpClient;
+import com.example.anish.prodapp.dto.EmpDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ProdappApplicationTests {
 
+	@Autowired
+	EmpClient empClient;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void getAllEmp(){
+		List<EmpDto> empDtoList = empClient.getAllEmployee();
+		System.out.println(empDtoList);
 	}
 
 }
